c4439e165e864b0cc424c1f06ec17da6f026f6fd
Jonathan Geis <jonathangeis@gmail.com>
2017-07-19 18:53:52 -0400

    User Input is operational

--- app/products_app.py
+++ app/products_app.py
@@ -1,1 +1,24 @@
-print("Welcome to the Netherworld!")
+menu = """
+
+    Welcome to the products app.
+
+    There are 28 products.
+
+    Please choose an operation: 'List', 'Show', 'Create', 'Update', 'Destroy'
+
+"""
+
+chosen_operation = input(menu)
+
+if chosen_operation.title() == "List":
+    print("LISTING PRODUCTS")
+elif chosen_operation.title() == "Show":
+    print("SHOWING PRODUCTS")
+elif chosen_operation.title() == "Create":
+    print("CREATING PRODUCTS")
+elif chosen_operation.title() == "Update":
+    print("UPDATING PRODUCTS")
+elif chosen_operation.title() == "Destroy":
+    print("DESTROYING PRODUCTS")
+else:
+     print("WRONG COMMAND")           
