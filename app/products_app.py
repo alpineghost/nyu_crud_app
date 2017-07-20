cde88835e2e31638d69d7cbebd48c7e45a7badf8
Jonathan Geis <jonathangeis@gmail.com>
2017-07-19 21:09:17 -0400

    Create new product

--- app/products_app.py
+++ app/products_app.py
@@ -1,15 +1,19 @@
 
-#Reading CSV Files
+
 
 import csv
 
+#Reading CSV Files
+csv_file_path = "data/products.csv"
+with open(csv_file_path, "r") as csv_file:
+    reader = csv.DictReader(csv_file) # assuming your CSV has headers, otherwise... csv.reader(csv_file)
+    for row in reader:
+        print(row["id"], row["name"])
 
 ###counting rows from Prof Rossetti
 products = []
 
 csv_file_path = "data/products.csv"
-
-##Reading csv file
 with open(csv_file_path, "r") as csv_file:
     reader = csv.DictReader(csv_file)
     for row in reader:
@@ -17,11 +21,12 @@ with open(csv_file_path, "r") as csv_file:
 
 other_path = "data/other_products.csv"
 ##writing csv file
-with open(other_path, "w", newline = '') as csv_file:
-    writer = csv.DictWriter(csv_file, fieldnames=["id", "name", "aisle","department","price"])
-    writer.writeheader() # uses fieldnames set above
-    for product in products:
-        writer.writerow(product)
+#with open(other_path, "w", newline = '') as csv_file:
+#    writer = csv.DictWriter(csv_file, fieldnames=["id", "name", "aisle","department","price"])
+#    writer.writeheader() # uses fieldnames set above
+#    for product in products:
+#        writer.writerow(product)
+
 
 
 menu = """
@@ -34,10 +39,10 @@ menu = """
 
 """.format(len(products))
 
-print(menu)
+#print(menu)
 
-#chosen_operation = input(menu)
-#chosen_operation = chosen_operation.title()
+chosen_operation = input(menu)
+chosen_operation = chosen_operation.title()
 
 def list_products():
     print("LISTING PRODUCTS")
@@ -47,6 +52,23 @@ def show_product():
 
 def create_product():
     print("CREATING A PRODUCT")
+    product_name = input("Enter the new product's name: ")
+    product_aisle = input("Enter the new product's aisle: ")
+    product_department = input("Enter the new product's department: ")
+    product_price = input("Enter the new product's price: ")
+    new_product = {
+        "id": len(products)+ 1 ,
+        "name": product_name,
+        "aisle": product_aisle,
+        "department": product_department,
+        "price": product_price
+    }
+    print("NEW PRODUCT IS", new_product)
+    products.append(new_product)
+
+
+
+
 
 def update_product():
     print("UPDATING A PRODUCT")
@@ -54,26 +76,21 @@ def update_product():
 def destroy_product():
     print("DESTROYING A PRODUCT")
 
-#if chosen_operation == "List":
-#    list_products()
-#elif chosen_operation == "Show":
-#    show_product()
-#elif chosen_operation == "Create":
-#    create_product()
-#elif chosen_operation == "Update":
-#    update_product()
-#elif chosen_operation == "Destroy":
-#    destroy_product()
-#else:
-#     print("WRONG COMMAND. PLEASE TRY AGAIN.")
+if chosen_operation == "List":
+    list_products()
+elif chosen_operation == "Show":
+    show_product()
+elif chosen_operation == "Create":
+    create_product()
+elif chosen_operation == "Update":
+    update_product()
+elif chosen_operation == "Destroy":
+    destroy_product()
+else:
+     print("WRONG COMMAND. PLEASE TRY AGAIN.")
 
-#Reading CSV Files
-
-import csv
-
-csv_file_path = "data/products.csv"
-
-with open(csv_file_path, "r") as csv_file:
-    reader = csv.DictReader(csv_file) # assuming your CSV has headers, otherwise... csv.reader(csv_file)
-    for row in reader:
-        print(row["id"], row["name"])
+with open(csv_file_path, "w", newline = '') as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=["id", "name", "aisle","department","price"])
+    writer.writeheader() # uses fieldnames set above
+    for product in products:
+        writer.writerow(product)     
